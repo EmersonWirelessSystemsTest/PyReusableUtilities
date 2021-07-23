8040a1fc2fc942a95f0136a48912e191aed9ec64
Alex Stangl <67007903+AlexStanglEmerson@users.noreply.github.com>
2021-07-23 11:46:52 -0500

    Update PathUtils.py

--- PyReusableUtilities/PathUtils.py
+++ PyReusableUtilities/PathUtils.py
@@ -9,7 +9,7 @@ def walk_files(path: str) -> str:
             yield os.path.join(dirpath, filename)
 
 
-def add_folders_to_path(starting_file: str, levels_up: int = 0) -> None:
+def add_folders_to_path(starting_file: str, levels_up: int = 0, prefixes_to_skip: list = ["."]) -> None:
     """
     Modifies the Python path with a folder and all its child folders to help with submodule imports
     A file is passed to the function, the parent folder is extracted, then the folder is moved up by levels_up
@@ -18,6 +18,7 @@ def add_folders_to_path(starting_file: str, levels_up: int = 0) -> None:
     Parameters:
     starting_file (str): The value passed to this should almost always be the built-in variable __file__
     levels_up (int): How many directories up from the starting_file should be traversed
+    prefixes_to_skip (list): A list of strings indicating if a folder should be skipped if any of the folders start with one of the prefixes
     """
     starting_path = Path(starting_file)
     starting_path = starting_path.parent if starting_path.is_file() else starting_path
@@ -28,4 +29,11 @@ def add_folders_to_path(starting_file: str, levels_up: int = 0) -> None:
     for dirpath, dirnames, filenames in os.walk(starting_path):
         for dirname in dirnames:
             path_to_add = Path(dirpath, dirname)
-            sys.path.append(path_to_add)
+
+            add_to_path = True
+            for part in path_to_add.parts:
+                if part.startswith(tuple(prefixes_to_skip)):
+                    add_to_path = False
+            
+            if add_to_path:
+                sys.path.append(str(path_to_add))
