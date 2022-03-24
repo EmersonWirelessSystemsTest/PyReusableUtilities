ef110769c92e555aceb12dd31be38731cf395cf8
Alex Stangl <67007903+AlexStanglEmerson@users.noreply.github.com>
2022-03-24 08:52:19 -0500

    Added an Optional Timeout Parameter when Acquiring a File Lock
    To prevent programs from freezing, an optional timeout can be supplied when acquiring a file lock. If the default of None is used, the program will try to acquire a lock for 100 years. Otherwise, it will wait for timeout_sec seconds before raising a TimeoutError.

--- PyReusableUtilities/FileUtils.py
+++ PyReusableUtilities/FileUtils.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from datetime import datetime, timedelta
 from os import PathLike, remove
 from pathlib import Path
 from time import sleep
@@ -6,22 +7,44 @@ from typing import Union, ContextManager
 
 
 @contextmanager
-def acquire_file_lock(filepath: Union[str, PathLike]) -> ContextManager[None]:
+def acquire_file_lock(filepath: Union[str, PathLike], timeout_sec: float = None) -> ContextManager[None]:
     """
     Acquires a lock on a file (through a <filepath>.lock file) before yielding control back to the program.
     This ensures multiple threads, processes, or programs trying to access the same file do not encounter a race condition.
 
     This function MUST be called as a context manager (using the "with" keyword) to ensure setup and cleanup are done correctly.
+    No object is returned by the context manager.
+
+    Parameters:
+    filepath (PathLike): The path to the file for which a lock is to be acquired.
+    timeout_sec (float): An optional timeout (in seconds) on how long to wait for the file to avoid getting stuck.
+
+    Raises:
+    TimeoutError: If the lock cannot be acquired within timeout_sec seconds, a TimeoutError will be raised.
+
+    Yields:
+    None
     """
+    # Calculate the datetime at which a TimeoutError should be raised (a timeout of None is actually just 100 years in the future)
+    timeout_dt = datetime.now() + timedelta(days = 365 * 100)
+    if timeout_sec is not None:
+        timeout_dt = datetime.now() + timedelta(seconds = timeout_sec)
+    timeout_msg = "A lock could not be acquired before the specified timeout"
+
     # Keep polling to see until the lock file does not exist (with short sleep durations between polls)
     path = Path(filepath)
     lock_path = Path(str(path) + ".lock")
     while True:
         try:
             while lock_path.exists():
+                if datetime.now() > timeout_dt:
+                    raise TimeoutError(timeout_msg)
                 sleep(0.01)
+
             break
         except PermissionError:
+            if datetime.now() > timeout_dt:
+                raise TimeoutError(timeout_msg)
             sleep(0.01)
 
     # Create the lock file and yield control back to the user
@@ -30,6 +53,8 @@ def acquire_file_lock(filepath: Union[str, PathLike]) -> ContextManager[None]:
             lock_path.touch(exist_ok = False)
             break
         except (FileExistsError, PermissionError):
+            if datetime.now() > timeout_dt:
+                raise TimeoutError(timeout_msg)
             sleep(0.01)
     yield
 
